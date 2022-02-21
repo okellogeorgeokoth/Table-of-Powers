8dd112b3fa60866632db9c34df43f00a0f2d76ee
okellogeorgeokoth <okoth603@gmail.com>
2022-02-21 03:07:34 +0300

    Table-of-Powers

---
+++ Form1.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtA = New System.Windows.Forms.TextBox()
+        Me.btnDisplay = New System.Windows.Forms.Button()
+        Me.powerListBox = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(58, 42)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(150, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter Upper limit as Integer"
+        '
+        'txtA
+        '
+        Me.txtA.Location = New System.Drawing.Point(61, 71)
+        Me.txtA.Name = "txtA"
+        Me.txtA.Size = New System.Drawing.Size(203, 23)
+        Me.txtA.TabIndex = 1
+        '
+        'btnDisplay
+        '
+        Me.btnDisplay.Location = New System.Drawing.Point(299, 71)
+        Me.btnDisplay.Name = "btnDisplay"
+        Me.btnDisplay.Size = New System.Drawing.Size(172, 23)
+        Me.btnDisplay.TabIndex = 2
+        Me.btnDisplay.Text = "Display Table of Powers"
+        Me.btnDisplay.UseVisualStyleBackColor = True
+        '
+        'powerListBox
+        '
+        Me.powerListBox.FormattingEnabled = True
+        Me.powerListBox.ItemHeight = 15
+        Me.powerListBox.Location = New System.Drawing.Point(61, 124)
+        Me.powerListBox.Name = "powerListBox"
+        Me.powerListBox.Size = New System.Drawing.Size(410, 169)
+        Me.powerListBox.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(509, 333)
+        Me.Controls.Add(Me.powerListBox)
+        Me.Controls.Add(Me.btnDisplay)
+        Me.Controls.Add(Me.txtA)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "Form1"
+        Me.Text = "Table of Powers"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtA As TextBox
+    Friend WithEvents btnDisplay As Button
+    Friend WithEvents powerListBox As ListBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,61 @@
+﻿'Name
+'Details
+Public Class Form1
+    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+        Dim a As Integer 'Declaring a as integer value
+        a = Val(txtA.Text) 'Assigning a to textbox
+
+
+        ' Compute only if passed value is positive and greater than 0
+
+        If a > 0 Then
+
+            ' variable for loop
+
+            Dim powerCounter As Integer = 1
+
+            ' clear previously stored values
+
+            powerListBox.Items.Clear()
+
+            ' display the heading in the ListBox
+
+            powerListBox.Items.Add("n" + vbTab + "n^2" + vbTab + "n^3")
+
+            Dim square As Integer = 1
+
+            Dim cube As Integer = 1
+
+            ' loop till all values are printed
+
+            Do While powerCounter <= a
+
+                square = powerCounter * powerCounter
+
+                cube = powerCounter * powerCounter * powerCounter
+
+                ' compute the powers and add to the ListBox
+
+                powerListBox.Items.Add(powerCounter.ToString + vbTab + square.ToString + vbTab + cube.ToString)
+
+                ' increment counter
+
+                powerCounter += 1
+
+            Loop
+
+            ' if entered value is 0 or less than display an error message
+
+        Else
+
+            ' clear the content of ListBox
+
+            powerListBox.Items.Clear()
+
+            ' add an error message
+
+            powerListBox.Items.Add("Enter a valid" & " number > 0")
+
+        End If
+    End Sub
+End Class
